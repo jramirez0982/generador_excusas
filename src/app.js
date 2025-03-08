3fbd4a0bbea59383e43bd86afce9818792fbeaf5
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-03-08 23:25:00 +0000

    first comment

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let quien = ['El perro', 'Mi abuela', 'El mensajero', 'Mi pajaro'];
+  let accion = ['perdio', 'rompio', 'aplasto', 'daño'];
+  let que = ['mi tarea', 'mi telefono', 'el carro', 'la ropa'];
+  let cuando = ['antes de la clase', 'mientras dormia', 'mientras hacia ejercicio', 'mientras caminaba' ];
+
+  let indQuien = Math.floor(Math.random()*4);
+  let indAccion = Math.floor(Math.random()*4);
+  let indQue = Math.floor(Math.random()*4);
+  let indCuando = Math.floor(Math.random()*4);
+  
+  let mensaje = quien[indQuien] + " " + accion[indAccion] + " " + que[indQue] + " " + cuando[indCuando];
+  
+  
+  document.querySelector('#excusa').innerHTML=mensaje;
   console.log("Hello Rigo from the console!");
+  
 };
